4855c658e7a967340e531dbc8c346f8e113a89bb
Elton Ferreira de Souza <elton@MacBook-Air-de-Elton.local>
2024-04-15 01:35:51 -0300

    adding new files

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from flask import Flask
 from openai import OpenAI
+from myfilter.prompts import Prompt
 
 client = OpenAI()
 
 app = Flask(__name__)
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+def request_message(prompt):
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": str(prompt)}
+    ]
+    )
+    return completion
 
 #breakpoint()
 
 @app.route("/")
 def hello_world():
+    message = "Are you dumb? the deadline was next week not now"
+    prompt = Prompt.good_guy(message=message)
+    print (prompt)
+    completion = request_message(prompt)
     return "<p>"+str(completion.choices[0].message.content)+"</p>"
---
+++ prompts.py
@@ -0,0 +1,6 @@
+class Prompt:
+    def __init__(self):
+        self.prompt = None
+    def good_guy(message):
+        prompt = f"I want you to rewrite the following message as formal as possible removing any bad words and changing insultes to compliments: '{message}'"
+        return prompt
